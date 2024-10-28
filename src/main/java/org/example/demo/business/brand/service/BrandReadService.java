d07ff0fbf72baa1d2e7e45886c4d6cc3775fe389
권덕주 <djgwon@lifegoeson.kr>
2024-10-28 20:37:03 +0900

    transactional 추가

--- src/main/java/org/example/demo/business/brand/service/BrandReadService.java
+++ src/main/java/org/example/demo/business/brand/service/BrandReadService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.demo.business.brand.Brand;
 import org.example.demo.infrastructure.repository.brand.BrandReadRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -13,10 +14,12 @@ public class BrandReadService {
 
     private final BrandReadRepository brandReadRepository;
 
+    @Transactional(readOnly = true)
     public Brand findById(final Long id) {
         return brandReadRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Brand not found"));
     }
 
+    @Transactional(readOnly = true)
     public Set<Brand> findAllByIds(final Set<Long> ids) {
         return brandReadRepository.findAllByIds(ids);
     }
